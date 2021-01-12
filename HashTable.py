f8d2c3cbd18862e332885e747266f8df8a22d0f8
unknown <akshat.aksharma@gmail.com>
2021-01-13 01:17:01 +0530

    Adding solution for collision using chaining methode

--- HashTable.py
+++ HashTable.py
@@ -1,7 +1,7 @@
 class HashTable:
     def __init__(self):
         self.max = 100 #for initiating 100 sized list 
-        self.arr =[None for i in range(self.max)]
+        self.arr =[ [] for i in range(self.max)]
     def hash_key(self,key): # funtion for finding the hash key using ASCII 
         h = 0 # this is varialbe for saving the sum
         for char in key:
@@ -10,14 +10,24 @@ class HashTable:
         return h % self.max # assuming that 100 will the size of the list so we are calculating mod using 100
     def add(self,key,value):
         h = self.hash_key(key)
-        self.arr[h]= value
+        found = False
+        for idx,element in enumerate(self.arr[h]):
+            if element[0]== key and len(element) == 2:
+                self.arr[h][idx] = (key,value)
+                found = True 
+                break
+        if not found:
+            self.arr[h].append((key,value))
     def get(self,key):
         h = self.hash_key(key)
-        #print(self.arr[h])
-        return self.arr[h]
+        for each in self.arr[h]:
+            if each[0] == key:
+                #print(each[1]) 
+                return each[1]
         
 if __name__ == '__main__':
     t = HashTable() 
-    t.hash_key('march 6')
     t.add('march 6',310)
+    t.add('march 17',400)
+    t.add('march 6',489)
     t.get('march 6')
